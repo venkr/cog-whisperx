0497bbf5daaa8456ae5b2ea5f085dba92e8264ee
Venki Kumar <venkateshrameshkumar+1@gmail.com>
2024-02-10 12:28:11 -0800

    Add diarization

--- predict.py
+++ predict.py
@@ -8,6 +8,7 @@ import torch
 import whisperx
 import json
 
+#HF_TOKEN = "add_your_token"
 
 compute_type="float16"
 class Predictor(BasePredictor):
@@ -15,24 +16,28 @@ class Predictor(BasePredictor):
         """Load the model into memory to make running multiple predictions efficient"""
         self.device = "cuda"
         self.model = whisperx.load_model("large-v2", self.device, language="en", compute_type=compute_type)
-        self.alignment_model, self.metadata = whisperx.load_align_model(language_code="en", device=self.device)
+        #self.alignment_model, self.metadata = whisperx.load_align_model(language_code="en", device=self.device)
+        self.diarize_model = whisperx.DiarizationPipeline(use_auth_token=HF_TOKEN, device=self.device)
 
     def predict(
         self,
         audio: Path = Input(description="Audio file"),
         batch_size: int = Input(description="Parallelization of input audio transcription", default=32),
-        align_output: bool = Input(description="Use if you need word-level timing and not just batched transcription", default=False),
+        diarize: bool = Input(description="Include speaker diarization", default=True),
+        #align_output: bool = Input(description="Use if you need word-level timing and not just batched transcription", default=False),
         only_text: bool = Input(description="Set if you only want to return text; otherwise, segment metadata will be returned as well.", default=False),
         debug: bool = Input(description="Print out memory usage information.", default=False)
     ) -> str:
         """Run a single prediction on the model"""
         with torch.inference_mode():
-            result = self.model.transcribe(str(audio), batch_size=batch_size) 
+            audio = whisperx.load_audio(str(audio))
+            result = self.model.transcribe(audio, batch_size=batch_size) 
             # result is dict w/keys ['segments', 'language']
             # segments is a list of dicts,each dict has {'text': <text>, 'start': <start_time_msec>, 'end': <end_time_msec> }
-            if align_output:
+            
+            #if align_output:
                 # NOTE - the "only_text" flag makes no sense with this flag, but we'll do it anyway
-                result = whisperx.align(result['segments'], self.alignment_model, self.metadata, str(audio), self.device, return_char_alignments=False)
+                #result = whisperx.align(result['segments'], self.alignment_model, self.metadata, audio, self.device, return_char_alignments=False)
                 # dict w/keys ['segments', 'word_segments']
                 # aligned_result['word_segments'] = list[dict], each dict contains {'word': <word>, 'start': <start_time_msec>, 'end': <end_time_msec>, 'score': probability}
                 #   it is also sorted
@@ -40,6 +45,13 @@ class Predictor(BasePredictor):
                 # return_char_alignments adds in character level alignments. it is: too many. 
             if only_text:
                 return ''.join([val.text for val in result['segments']])
+            
+            if diarize:
+                diarize_segments = self.diarize_model(audio)
+
+                # merge diarization segments with text segments
+                result = whisperx.assign_word_speakers(diarize_segments, result)
+
             if debug:
                 print(f"max gpu memory allocated over runtime: {torch.cuda.max_memory_reserved() / (1024 ** 3):.2f} GB")
         return json.dumps(result['segments'])
